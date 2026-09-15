8a2cf8f4362c96a00c9337b1d7ac60c256a1000d
Joliyuh <imyourjulie28@gmail.com>
2026-09-15 13:40:37 +0800

    Added Activity Logging and Sign Out

--- app/admin/index.php
+++ app/admin/index.php
@@ -1,13 +1,19 @@
+<?php
+require '../../config/config.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin</title>
 </head>
 <body>
+
     <h1>Hello Admin</h1>
-<a href="<?= BASE_URL ?>/auth/signout.php">Sign Out</a>
-    
+
+    <a href="<?= BASE_URL ?>/auth/signout.php">Sign Out</a>
+
 </body>
 </html>
--- app/user/index.php
+++ app/user/index.php
@@ -1,11 +1,19 @@
+<?php
+require '../../config/config.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>User</title>
 </head>
 <body>
+
     <h1>Hello User</h1>
+
+    <a href="<?= BASE_URL ?>/auth/signout.php">Sign Out</a>
+
 </body>
 </html>
